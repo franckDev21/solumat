24b7b1e428903a49cc4e8fd6c2ba537e9db88c8c
franckDev21 <tutodevphp@gmail.com>
2022-09-26 19:11:29 +0100

    update app

--- resources/views/components/prestations-service.blade.php
+++ resources/views/components/prestations-service.blade.php
@@ -4,18 +4,18 @@
   </h1>
   <h2 class="text-4xl text-center font-bold mt-5 mb-2">Nos Prestations Professionnelles</h2>
   <p class="text-center mb-4">
-      Nous avons plusieurs types de prestations pour faciliter la réussite de vos événements.
+      Nous avons plusieurs types de prestations pour faciliter la réussite de votre projet.
   </p>
   <div class="container px-10 text-center">
       <div class="grid grid-cols-3 gap-6">
-          <div class="bg-white shadow-md">
+          <div class="bg-white shadow-md rounded-md overflow-hidden">
               <div class="relative h-[180px]"><img class="absolute w-full h-full object-cover" src="{{ asset('img/banner.jpg') }}" alt=""></div>
               <div class="p-4">
                   <h1 class="text-xl mb-3 font-bold after:block after:w-[40%] after:mt-1 after:h-[2px] after:rounded-md after:opacity-20 after:bg-secondary after:mx-auto">Création & Entretien des piscines</h1>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut distinctio itaque facere, nobis eius aspernatur.</p>
               </div>    
           </div>
-          <div class="bg-white shadow-md">
+          <div class="bg-white shadow-md rounded-md overflow-hidden">
               {{-- <div class="relative h-[180px]"><img class="absolute w-full h-full object-cover" src="{{ asset('img/produits-piscine.jpg') }}" alt=""></div> --}}
 
               <div class="relative h-[180px]"><img class="absolute w-full h-full object-cover" src="{{ asset('img/v2.jpeg') }}" alt=""></div>
@@ -24,28 +24,28 @@
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut distinctio itaque facere, nobis eius aspernatur.</p>
               </div>    
           </div>
-          <div class="bg-white shadow-md">
+          <div class="bg-white shadow-md rounded-md overflow-hidden">
               <div class="relative h-[180px]"><img class="absolute w-full h-full object-cover" src="{{ asset('img/RAISONS1C.jpg') }}" alt=""></div>
               <div class="p-4">
                   <h1 class="text-xl mb-3 font-bold after:block after:w-[40%] after:mt-1 after:h-[2px] after:rounded-md after:opacity-20 after:bg-secondary after:mx-auto">Plomberie</h1>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut distinctio itaque facere, nobis eius aspernatur.</p>
               </div>    
           </div>
-          <div class="bg-white shadow-md">
+          <div class="bg-white shadow-md rounded-md overflow-hidden">
               <div class="relative h-[180px]"><img class="absolute w-full h-full object-cover" src="{{ asset('img/b3.jpg') }}" alt=""></div>
               <div class="p-4">
                   <h1 class="text-xl mb-3 font-bold after:block after:w-[40%] after:mt-1 after:h-[2px] after:rounded-md after:opacity-20 after:bg-secondary after:mx-auto">Réalisation des forages & Traitement des eaux</h1>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut distinctio itaque facere, nobis eius aspernatur.</p>
               </div>    
           </div>
-          <div class="bg-white shadow-md">
+          <div class="bg-white shadow-md rounded-md overflow-hidden">
               <div class="relative h-[180px]"><img class="absolute w-full h-full object-cover" src="{{ asset('img/PEINTRE.jpg') }}" alt=""></div>
               <div class="p-4">
                   <h1 class="text-xl mb-3 font-bold after:block after:w-[40%] after:mt-1 after:h-[2px] after:rounded-md after:opacity-20 after:bg-secondary after:mx-auto">Peinture bâtiment</h1>
                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut distinctio itaque facere, nobis eius aspernatur.</p>
               </div>    
           </div>
-          <div class="bg-white shadow-md">
+          <div class="bg-white shadow-md rounded-md overflow-hidden">
               <div class="relative h-[180px]"><img class="absolute w-full h-full object-cover" src="{{ asset('img/v1.jpg') }}" alt=""></div>
               <div class="p-4">
                   <h1 class="text-xl mb-3 font-bold after:block after:w-[40%] after:mt-1 after:h-[2px] after:rounded-md after:opacity-20 after:bg-secondary after:mx-auto">Vidange des fosses</h1>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
                 <h1 class=" text-secondary font-bold">Pourquoi Nous ?</h1>
                 <h2 class="text-5xl font-bold my-6 text-tersiaire ">Nous sommes a votre service pour la reuisite de vos projets </h2>
                 <p class="w-[80%] my-10">
-                    METOGUEZ GROUP est une entreprise de Services Professionnels avec une Expertise Avérée dans divers secteurs d’activité, notament en  Conseil & Gestion, Nettoyage et Services, Services à la Personne.
+                    SOLUMAT SARL est une entreprise de Services Professionnels avec une Expertise Avérée dans divers secteurs d’activité, notament en  Conseil & Gestion, Nettoyage et Services, Services à la Personne.
                 </p>
                 <div>
                     <button class="nav-btn bg-secondary bg-opacity-80 uppercase font-bold border-4 border-secondary text-white">Nos réalisations <i class="fa-solid fa-globe ml-4"></i></button>
